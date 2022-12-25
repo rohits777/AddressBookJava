1a852a465e20b13c217f732182f582212f1023ae
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-12-26 00:28:19 +0530

    Added: UC6 Multiple address book.

--- src/com/bridgelabz/Contacts.java
+++ src/com/bridgelabz/Contacts.java
@@ -77,12 +77,12 @@ public class Contacts {
     @Override
     public String toString() {
         return "Contacts Details :" +"\n"+
-                "Firstname :" + firstname +","+
-                "Lastname :" + lastname + ','+
-                "Address :" + Address + ',' +
-                "City :" + city + ',' +
-                "State :" + state + ',' +
-                "Pin :" + Pin +','+
+                "Firstname :" + firstname +"\n"+
+                "Lastname :" + lastname + '\n'+
+                "Address :" + Address + '\n' +
+                "City :" + city + '\n' +
+                "State :" + state + '\n' +
+                "Pin :" + Pin +'\n'+
                 "PhoneNumber :" + phonenumber +','+
                 "Email :" + email ;
     }
