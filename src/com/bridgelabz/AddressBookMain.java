45a94660e00d8dde1b772a063058b55cec0bb2f4
Rohit <Rohit@spotblhol0128.awsspoton.com>
2023-01-05 18:35:40 +0530

    Added: UC7 Duplicate Entry avoid.

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,35 +1,39 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("--------Welcome to Address Book Program-------");
+        Scanner sc=new Scanner(System.in);
         int choice;
+        AddressBook addressBook = new AddressBook();
         do {
-            System.out.println("1. Add New AddressBook \n2. Delete AddressBook" + "\n3. Display AddressBook\n4. Add Contact\n5. Exit");
+            System.out.println("1. Add New Contact\n2. Edit Contact\n3. Delete Contact" + "\n4. Display Contact\n5. Exit");
             System.out.println("Enter Choice: ");
-            choice = AddressBookRepo.userInput();
+            choice =sc.nextInt();
 
             switch (choice) {
                 case 1:
-                    AddressBookRepo.addNewAddressBook();  //Add Address Book
+                    addressBook.addNewContact();
                     break;
                 case 2:
-                    AddressBookRepo.deleteAddressBook();   //Delete Address Book
+                    addressBook.editContact();
                     break;
                 case 3:
-                    AddressBookRepo.displayAddressBooks();  //Display Address Book
+                    addressBook.deleteContact();
                     break;
                 case 4:
-                    AddressBookRepo.selectAddressBook();     //Select Address Book
+                    addressBook.displayContact();
                     break;
                 case 5:
                     System.out.println("Thank You for Using Address Book.");
                     break;
                 default:
-                    System.out.println("Please Select between 1 to 6 only.");
+                    System.out.println("Please Select between 1 to 5 only.");
                     break;
             }
         }
-        while( choice !=5 );
+        while( choice != 5 );
     }
 }
